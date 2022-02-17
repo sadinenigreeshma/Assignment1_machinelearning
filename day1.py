98231740d5b5875245dee6e53faf1e21583561e3
sadinenigreeshma <71624353+sadinenigreeshma@users.noreply.github.com>
2022-02-17 15:53:18 +0530

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+arra1=np.random.randint(-10,10,(3,5))
+print(arra1)
+
+D = np.array(range(18)) + 3
+x=D[1:10]
+y=D[9:0:-1]
+z=D[D.size::-1]
+###How are NumPy Arrays better than Lists in Python?  
+'''The arrays created with NumPy are more compact than Python Lists.
+Accessing arrays and reading and writing elements to arrays is comparatively faster than doing the same in Python Lists.
+The arrays created in NumPy are more efficient and convenient to use.NumPy provides more functionality than the list '''
+###Explain the data types supported by Numpy. 
+'''
+interger
+float
+string
+boolean
+unsigned interger
+datetime
+void
+'''
+
+
+
+
